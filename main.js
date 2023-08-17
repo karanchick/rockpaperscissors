2b1a06a70e4d9e13490f37181797251550960d18
karan <chickkaran@gmail.com>
2023-08-17 17:06:48 -0600

    Fix game function and add user prompt

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function getComputerChoice(opt) {
     const randomIndex = Math.floor(Math.random()* opt.length);
     const item = opt[randomIndex];
     return item;
-
 }
-
 const options = ["Rock", "Paper", "Scissors"];
 
 function playGame(playerSelection,computerSelection){
@@ -15,32 +13,49 @@ function playGame(playerSelection,computerSelection){
     let pSelection = firstLetter.toUpperCase() + restLetters.toLowerCase();
 
     (console.log(pSelection))
-    (console.log(computerSelection));
+    console.log(computerSelection);
 
     if (computerSelection === "Rock" && pSelection === "Paper") {
-        return ("You win! Paper beats Rock"); }
-
+        return 1 }//("You win! Paper beats Rock"); }
     if (computerSelection === "Paper" && pSelection === "Rock") {
-        return ("You lose! Paper beats Rock"); }
-    
+        return 0}//("You lose! Paper beats Rock"); }
     if (computerSelection === "Rock" && pSelection === "Scissors") {
-        return ("You lose! Rock beats Scissors");
-    }
+        return 0}//"You lose! Rock beats Scissors");}
     if (computerSelection === "Scissors" && pSelection === "Rock") {
-        return ("You win! Rock beats Scissors");
-    }
+        return 1}//("You win! Rock beats Scissors"); }
     if (computerSelection === "Paper" && pSelection === "Scissors") {
-        return ("You win! Scissors beats Paper");
-    }
+        return 1}//("You win! Scissors beats Paper");}
     if (computerSelection === "Scissors" && pSelection === "Paper") {
-        return ("You win! Paper beats Scissors");
-    }
+        return 0}//("You lose! Scissors beats Paper"); }
     if (computerSelection === pSelection) {
-        return ("It's a tie!");
-    }
+        return 3}//("It's a tie!");}
       
 }
+let playerSelection = prompt("Type your choice:")
+let computerSelection = getComputerChoice(options)
 
+let round = 0
+let pScore = 0
+let cScore = 0
 
+function game() {
+    playGame(playerSelection,computerSelection);
+    if (playGame(playerSelection,computerSelection)== 1) {
+        round++;
+        pScore++;
+        return "You won! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
+    }
+    if ((playGame(playerSelection,computerSelection)== 0)){
+        round++;
+        cScore++;
+        return "You lost! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
+    }
+    else {
+        round++;
+        return "It's a tie! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
+    }
+}
+
+console.log(game())
 
 
