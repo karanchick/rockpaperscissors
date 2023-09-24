bde0482a8fe216f34ed8b7d419aa2a78d897b671
karan <chickkaran@gmail.com>
2023-09-23 23:55:29 -0700

    Add rock button

--- main.js
+++ main.js
@@ -7,55 +7,68 @@ function getComputerChoice(opt) {
 }
 const options = ["Rock", "Paper", "Scissors"];
 
-function playGame(playerSelection,computerSelection){
-    let firstLetter = playerSelection.slice(0,1);
-    let restLetters = playerSelection.substr(1);
-    let pSelection = firstLetter.toUpperCase() + restLetters.toLowerCase();
+let pScore = 0
+let cScore = 0
+let round = 0
 
-    (console.log(pSelection))
-    console.log(computerSelection);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    playerSelection = 'Rock';
+    playRound();
+   
+})
 
-    if (computerSelection === "Rock" && pSelection === "Paper") {
+function matchup(playerSelection,computerSelection){
+    console.log("You selected: " + playerSelection);
+    console.log("CPU selected: " + computerSelection);
+
+    if (computerSelection === "Rock" && playerSelection === "Paper") {
         return 1 }//("You win! Paper beats Rock"); }
-    if (computerSelection === "Paper" && pSelection === "Rock") {
+    if (computerSelection === "Paper" && playerSelection === "Rock") {
         return 0}//("You lose! Paper beats Rock"); }
-    if (computerSelection === "Rock" && pSelection === "Scissors") {
+    if (computerSelection === "Rock" && playerSelection === "Scissors") {
         return 0}//"You lose! Rock beats Scissors");}
-    if (computerSelection === "Scissors" && pSelection === "Rock") {
+    if (computerSelection === "Scissors" && playerSelection === "Rock") {
         return 1}//("You win! Rock beats Scissors"); }
-    if (computerSelection === "Paper" && pSelection === "Scissors") {
+    if (computerSelection === "Paper" && playerSelection === "Scissors") {
         return 1}//("You win! Scissors beats Paper");}
-    if (computerSelection === "Scissors" && pSelection === "Paper") {
+    if (computerSelection === "Scissors" && playerSelection === "Paper") {
         return 0}//("You lose! Scissors beats Paper"); }
-    if (computerSelection === pSelection) {
+    if (computerSelection === playerSelection) {
         return 3}//("It's a tie!");}
       
 }
-let playerSelection = prompt("Type your choice:")
-let computerSelection = getComputerChoice(options)
 
-let round = 0
-let pScore = 0
-let cScore = 0
 
-function game() {
-    playGame(playerSelection,computerSelection);
-    if (playGame(playerSelection,computerSelection)== 1) {
-        round++;
-        pScore++;
-        return "You won! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
-    }
-    if ((playGame(playerSelection,computerSelection)== 0)){
-        round++;
-        cScore++;
-        return "You lost! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
-    }
-    else {
-        round++;
-        return "It's a tie! Your score is " + pScore + ". The CPU's score is " + cScore + "! And the round is " + round
-    }
-}
+function playRound() {
+        //let playerSelection = prompt("Type your choice:")
+        let computerSelection = getComputerChoice(options);
+        matchup(playerSelection,computerSelection);
+        let result = matchup(playerSelection, computerSelection);
+        if (result == 1) {
+            round++;
+            pScore++;
+            console.log( "You won! Your score is " + pScore + 
+            ". The CPU's score is " + cScore + "! And the round is " + round)
+        }
+        if (result == 0){
+            round++;
+            cScore++;
+            console.log( "You lost! Your score is " + pScore + 
+            ". The CPU's score is " + cScore + "! And the round is " + round)
+        }
+        if (result == 3){
+            round++;
+            console.log ("It's a tie! Your score is " + pScore + 
+            ". The CPU's score is " + cScore + "! And the round is " + round)
+        }
 
-console.log(game())
+    }
+
+
+
+
+
+
 
 
